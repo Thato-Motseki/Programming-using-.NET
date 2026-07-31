7f33de67fe318a33d12edcdea0a67cf79a271278
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-31 09:33:39 +0200

    Add Pattern 6 - Inverted Pyramid Pattern

---
+++ Exercises/Pattern6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            for (int i = rows; i >= 1; i--)
+            {
+                // Print leading spaces
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print stars
+                for (int j = 1; j <= (2 * i - 1); j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
