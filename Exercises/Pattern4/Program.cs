d3a4f7a90491dcd93edf8f907c765b6df6a9b838
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-31 09:30:16 +0200

    Add Pattern 4 - Right Triangle Pattern

---
+++ Exercises/Pattern4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                // Print leading spaces
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write("  ");
+                }
+
+                // Print stars
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
