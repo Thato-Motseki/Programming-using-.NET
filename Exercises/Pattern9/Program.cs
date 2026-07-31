128cefc4ef90c7cff5d3b15baed425f3cc908753
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-31 09:36:24 +0200

    Add Pattern 9 - Hollow Triangle Pattern

---
+++ Exercises/Pattern9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j == 1 || j == i || i == rows)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
