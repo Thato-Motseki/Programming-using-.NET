0e56833b2b49bcdd0024f84de8882111ddce84ea
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-31 09:29:22 +0200

    Add Pattern 3 - Inverted Left Triangle Pattern

---
+++ Exercises/Pattern3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            for (int i = rows; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
