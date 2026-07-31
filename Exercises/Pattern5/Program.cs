94722e49db359cd1baaedabbaf71b13763597957
Thato-Motseki <thatomotseki292@gmail.com>
2026-07-31 09:32:44 +0200

    Add Pattern 5 - Centered Pyramid Pattern

---
+++ Exercises/Pattern5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                // Print leading spaces
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print stars
+                for (int j = 1; j <= (2 * i - 1); j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
