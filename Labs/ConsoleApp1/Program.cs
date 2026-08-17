dc954e0bd86d8942981057f9c03fa81dd75d7fce
Thato-Motseki <thatomotseki292@gmail.com>
2026-08-17 23:28:20 +0200

    feat: add package delivery tracking workflow

---
+++ Labs/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Package
+    {
+        public string TrackingNumber { get; set; }
+        public string Status { get; private set; }
+
+        public Package(string trackingNumber)
+        {
+            TrackingNumber = trackingNumber;
+            Status = "Created";
+        }
+
+        public void UpdateStatus(string newStatus)
+        {
+            Status = newStatus;
+        }
+    }
+}
