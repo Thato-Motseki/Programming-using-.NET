aa21ca4954019100a5072be70e1bef81dabd4bff
Thato-Motseki <thatomotseki292@gmail.com>
2026-08-22 22:49:31 +0200

    feat: create simple C# todo list

---
+++ Projects/todo list/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> tasks = new List<string>();
+
+        Console.WriteLine("=== My To-Do List ===");
+
+        tasks.Add("Study C#");
+        tasks.Add("Complete GitHub commit");
+        tasks.Add("Review OOP");
+
+        foreach (string task in tasks)
+        {
+            Console.WriteLine("- " + task);
+        }
+
+        Console.WriteLine($"\nTotal tasks: {tasks.Count}");
+    }
+}
